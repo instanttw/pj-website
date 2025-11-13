e73ea9a53d05e6735b1e12ef9888f8f46a2dd92a
PrintJones <dev@printjones.com>
2025-11-13 10:22:28 +0000

    fix(build): set Stripe API version to supported '2023-08-16' to satisfy types

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-const apiVersion: Stripe.LatestApiVersion = '2023-10-16'
+const apiVersion: Stripe.LatestApiVersion = '2023-08-16'
 
 export function getStripeClient() {
   const key = process.env.STRIPE_SECRET_KEY
